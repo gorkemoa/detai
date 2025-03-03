3c631eca6d08ab5821835b6a7c3f4fa911768286
gorkemoa <gorkemoa35@gmail.com>
2025-03-03 11:18:51 +0300

    düzenlemeler yapıldı

--- src/components/ui/use-toast.tsx
+++ src/components/ui/use-toast.tsx
@@ -23,12 +23,13 @@ type ActionTypes = {
   REMOVE_TOAST: "REMOVE_TOAST"
 }
 
-const actionTypes: ActionTypes = {
+// ActionType tipini doğrudan tanımlıyoruz
+type ActionType = {
   ADD_TOAST: "ADD_TOAST",
   UPDATE_TOAST: "UPDATE_TOAST",
   DISMISS_TOAST: "DISMISS_TOAST",
   REMOVE_TOAST: "REMOVE_TOAST",
-} as const
+}
 
 let count = 0
 
@@ -37,23 +38,21 @@ function genId() {
   return count.toString()
 }
 
-type ActionType = typeof actionTypes
-
 type Action =
   | {
-      type: ActionType["ADD_TOAST"]
+      type: "ADD_TOAST"
       toast: ToasterToast
     }
   | {
-      type: ActionType["UPDATE_TOAST"]
+      type: "UPDATE_TOAST"
       toast: Partial<ToasterToast>
     }
   | {
-      type: ActionType["DISMISS_TOAST"]
+      type: "DISMISS_TOAST"
       toastId?: ToasterToast["id"]
     }
   | {
-      type: ActionType["REMOVE_TOAST"]
+      type: "REMOVE_TOAST"
       toastId?: ToasterToast["id"]
     }
 
